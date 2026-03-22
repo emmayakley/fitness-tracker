624fd277ab8c8e8010f5d233d88716d515189542
emmayakley <emmie.yakley@gmail.com>
2026-03-22 14:52:48 -0700

    added vercel to CORS configuration

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const cors = require('cors');
 const app = express();
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://fitness-tracker-tpxy.vercel.app',
+    ],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
